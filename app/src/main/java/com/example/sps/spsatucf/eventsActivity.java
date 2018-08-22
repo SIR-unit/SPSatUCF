ced4f3b89b8b71724d01d718237f5f7fc366bef3
Zacchaeus Scheffer <zaccysc@gmail.com>
2018-08-21 21:23:30 -0400

    no longer crashes

--- app/src/main/java/com/example/sps/spsatucf/eventsActivity.java
+++ app/src/main/java/com/example/sps/spsatucf/eventsActivity.java
@@ -3,6 +3,9 @@ package com.example.sps.spsatucf;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class eventsActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +13,8 @@ public class eventsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
 
-        db = FirebaseFirestore.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
     }
 }
